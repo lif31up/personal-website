48d46f7afad12a86716d629adcbc31b8c9539924
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-03-03 11:11:45 +0900

    change bottombar

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,12 @@
 import GithubRepos from "@/component/feature/Gitub/GithubRepos";
 import GithubProfile from "@/component/feature/Gitub/GithubProfile";
 import { RecoilRoot } from "recoil";
-import TailProperties, { cn } from "@/styles/TailProperties";
 import Interest from "@/component/feature/Interest";
+import SVG from "@/component/common/SVG";
 
 export default function Home() {
-  const style: TailProperties = {
-    box: "pb-16",
-  };
   return (
-    <section className={cn(style)}>
+    <section>
       <RecoilRoot>
         <GithubProfile />
         <Interest />
@@ -19,6 +16,21 @@ export default function Home() {
           <GithubRepos />
         </section>
       </RecoilRoot>
+      <section className="w-full h-fit flex items-center justify-center py-4 mt-16 text-white gap-2">
+        <SVG
+          data={{
+            svg_url: "/profile.svg",
+            width: 8,
+            height: 8,
+            color: "white",
+            fill: "transparent",
+          }}
+        />
+        <h1 className="text-xs">Personal Website – @lif31up powered by 2025</h1>
+        <a className="text-xs" href="https://vercel.com/">
+          ▼ Vercel
+        </a>
+      </section>
     </section>
   );
 }
