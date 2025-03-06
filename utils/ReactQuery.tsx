d9bbee745568b916d0f31ea1ef476a6cc88bac82
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-03-06 11:28:27 +0900

    feat: improve RQ container pattern

--- utils/ReactQuery.tsx
+++ utils/ReactQuery.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { atom, RecoilState } from "recoil";
 
 function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -10,3 +11,63 @@ function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   );
 } // ReactQueryProvider_RENDER()
 export default ReactQueryProvider;
+
+// descData
+export const descDataAtom = atom<DescDataType | null>({
+  key: "desc",
+  default: null,
+}); // descDataAtom
+
+export type DescDataType = {
+  interests: InterestsDataType;
+  description: string;
+  projects: ProjectDataType[];
+}; // DescInterface
+export type InterestsDataType = {
+  desc: string;
+  topics: string[];
+}; // InterestsInterface
+export type ProjectDataType = {
+  name: string;
+  desc: string;
+  url0: string;
+  url1: string;
+  comment: string;
+}; // ProjectInterface
+
+export async function descDataQueryFn(setDesc: (data: DescDataType) => void) {
+  return await fetch(
+    `https://raw.githubusercontent.com/lif31up/lif31up.github.io/main/description.json`
+  ) // Fetch lif31up github profile
+    .then((response) => {
+      if (!response.ok) return null; // Return null if response isn't OK
+      return response.json(); // Parse JSON if successful
+    })
+    .then((data: DescDataType) => {
+      setDesc(data);
+      return data;
+    });
+} // descDataQueryFn:: success ? JSON : null
+
+// githubData
+
+export type GithubDataType = {
+  avatar_url: string;
+  login: string;
+  id: string;
+  location: string;
+  public_repos: string;
+  followers: string;
+  following: string;
+}; // PresenterDataType
+
+export async function githubDataQueryFn() {
+  return await fetch(`https://api.github.com/users/lif31up`) // Fetch lif31up github profile
+    .then((response) => {
+      if (!response.ok) return null; // Return null if response isn't OK
+      return response.json(); // Parse JSON if successful
+    })
+    .then((data) => {
+      return data; // Return parsed data
+    });
+} // githubDataQeuryFn:: success ? JSON : null
